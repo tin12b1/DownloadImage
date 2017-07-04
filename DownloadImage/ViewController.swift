30e5e6acf03152b258590047a497899d381deb3e
Tin Tran <tin12b1@gmail.com>
2017-07-04 23:32:25 +0700

    initial commit

--- DownloadImage/ViewController.swift
+++ DownloadImage/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var myImageView: UIImageView!
+    @IBOutlet var urlTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func downloadButtonClick(_ sender: Any) {
+        if (urlTextField.text == "") {
+            let alert = UIAlertController(title: "Error", message: "You must input url", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+        else {
+            let url = urlTextField.text
+            fetchImg(url!)
+        }
+    }
+    
+    func fetchImg(_ url: String) {
+        let imgUrl = URL(string: url)
+        (URLSession(configuration: .default)).dataTask(with: imgUrl!, completionHandler: {(dataImage, response, error) in
+            if let data = dataImage {
+                DispatchQueue.main.async() { () -> Void in
+                    self.myImageView.image = UIImage(data: data)
+                }
 
+            }
+        }).resume()
+    }
 }
 
